a31468af483140f5a107723dd3fec021ef2dd473
ThuHa307 <hatran3072003@gmail.com>
2024-03-27 23:58:46 +0700

    Configuration Prettier

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import { Routes, Route } from "react-router-dom";
-import { lazyLoad } from "./utils/lazyLoad";
-import { Suspense, Fragment } from "react";
-import Loader from "./pages/Loader/index.js";
-import DefaultLayout from "./layout/DefaultLayout/index.js";
-import { publicRoutes } from "./routes/index.js";
+import { Routes, Route } from 'react-router-dom';
+import { lazyLoad } from './utils/lazyLoad';
+import { Suspense, Fragment } from 'react';
+import Loader from './pages/Loader/index.js';
+import DefaultLayout from './layout/DefaultLayout/index.js';
+import { publicRoutes } from './routes/index.js';
 
 //const HomePage = lazyLoad(import("./pages/Homepage/index.js"));
 function App() {
-  return (
-    <Suspense fallback={<Loader />}>
-      <div className="App">
-        <Routes>
-          $
-          {publicRoutes.map((route, index) => {
-            const Page = route.component;
-            let Layout = DefaultLayout;
+    return (
+        <Suspense fallback={<Loader />}>
+            <div className="App">
+                <Routes>
+                    $
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
 
-            if (route.layout) {
-              Layout = route.layout;
-            } else if (route.layout === null) {
-              Layout = Fragment;
-            }
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={
-                  <Layout>
-                    <Page></Page>
-                  </Layout>
-                }
-              />
-            );
-          })}
-        </Routes>
-      </div>
-    </Suspense>
-  );
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page></Page>
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Suspense>
+    );
 }
 
 export default App;
